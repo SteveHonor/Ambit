3e2e7ba0136aca0105882a4724e4307f6daa3526
Steve J <steve@honors.com.br>
2015-01-19 23:06:58 -0200

    teste

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,22 @@ Bundler.require(*Rails.groups)
 
 module Ambit
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
+
+    initializer 'setup_asset_pipeline', :group => :all  do |app|
+    # We don't want the default of everything that isn't js or css, because it pulls too many things in
+    app.config.assets.precompile.shift
+
+    # Explicitly register the extensions we are interested in compiling
+    app.config.assets.precompile.push(Proc.new do |path|
+      File.extname(path).in? [
+        '.html', '.erb', '.haml',                 # Templates
+        '.png',  '.gif', '.jpg', '.jpeg',         # Images
+        '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+      ]
+    end)
   end
+
+
+  config.assets.initialize_on_precompile = false
+end
 end
